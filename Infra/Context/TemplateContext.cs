216c60a00237c3045d7cad74b21d530ee6f0b8bf
Matheus R <matheusrizzo@gmail.com>
2024-02-27 16:30:51 -0300

    Changing framework version

---
+++ Infra/Context/TemplateContext.cs
@@ -0,0 +1,20 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Context
+{
+    public class TemplateContext /*: DbContext  
+    {
+        public TemplateContext(DbContextOptions<TemplateContext> options)
+            :base(options)
+        {
+            
+        }
+
+        #region DBSets
+
+        public DbSet<User> { get; set; }
+
+        #endregion
+    }*/
+    { }
+}
